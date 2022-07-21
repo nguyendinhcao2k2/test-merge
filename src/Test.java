bcc191250f50b4cdea0daad10d639efbf5e5e986
caodinh <109230354+nguyendinhcao2k2@users.noreply.github.com>
2022-07-21 11:02:12 +0700

    Update Test.java
    C2 UPdate

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello EveryOne");
         System.out.println("hihi");
+        System.out.println("Đây Là DEV");
     }
 }
